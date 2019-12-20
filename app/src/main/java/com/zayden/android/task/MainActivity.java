12c21b6cfd0f402a5b06c61cededd533917b07a1
Peeyush Yadav <peeyushyadav1402@gmail.com>
2019-12-20 20:27:56 +0530

    Initial commit

---
+++ app/src/main/java/com/zayden/android/task/MainActivity.java
@@ -0,0 +1,126 @@
+package com.zayden.android.task;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RecycleAdapter adapter;
+    ArrayList<Todo> todoList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar_task_home);
+        setSupportActionBar(toolbar);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(MainActivity.this,TodoActivity.class);
+                MainActivity.this.startActivity(newIntent);
+            }
+        });
+
+        todoList = new ArrayList<>();
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        adapter = new RecycleAdapter();
+        recyclerView.setAdapter(adapter);
+
+        adapter.notifyDataSetChanged();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        database.getReference("todoList").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        todoList.clear();
+
+                        Log.w("TodoApp", "getUser:onCancelled " + dataSnapshot.toString());
+                        Log.w("TodoApp", "count = " + String.valueOf(dataSnapshot.getChildrenCount()) + " values " + dataSnapshot.getKey());
+                        for (DataSnapshot data : dataSnapshot.getChildren()) {
+                            Todo todo = data.getValue(Todo.class);
+                            todoList.add(todo);
+                        }
+
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.w("TodoApp", "getUser:onCancelled", databaseError.toException());
+                    }
+                });
+    }
+
+    private class RecycleAdapter extends RecyclerView.Adapter {
+
+
+        @Override
+        public int getItemCount() {
+            return todoList.size();
+        }
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_todo, parent, false);
+            SimpleItemViewHolder pvh = new SimpleItemViewHolder(v);
+            return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+            SimpleItemViewHolder viewHolder = (SimpleItemViewHolder) holder;
+            viewHolder.position = position;
+            Todo todo = todoList.get(position);
+            ((SimpleItemViewHolder) holder).title.setText(todo.getName());
+        }
+
+        public final  class SimpleItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            TextView title;
+            public int position;
+            public SimpleItemViewHolder(View itemView) {
+                super(itemView);
+                itemView.setOnClickListener(this);
+                title = itemView.findViewById(R.id.task_name);
+            }
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/zayden/android/task/TodoActivity.java
@@ -0,0 +1,73 @@
+package com.zayden.android.task;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo);
+    }
+
+    FloatingActionButton fab = findViewById(R.id.fab1);
+    fab.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            saveTodo();
+        }
+    }
+
+    void saveTodo() {
+        // first section
+        // get the data to save in our firebase db
+        EditText nameEdtText = (EditText)findViewById(R.id.task_name);
+        EditText descEditText = (EditText)findViewById(R.id.task_desc);
+        DatePicker datePicker = (DatePicker) findViewById(R.id.datepicker);
+        Date date = new Date();
+        date.setMonth(datePicker.getMonth());
+        date.setYear(datePicker.getYear());
+        date.setDate(datePicker.getDayOfMonth());
+
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        String dateString = format.format(date);
+
+
+
+        //second section
+        //save it to the firebase db
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        String key = database.getReference("todoList").push().getKey();
+
+        Todo todo = new Todo();
+        todo.setName(nameEdtText.getText().toString());
+        todo.setMessage(descEditText.getText().toString());
+        todo.setDate(dateString);
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put( key, todo.toFirebaseObject());
+        database.getReference("todoList").updateChildren(childUpdates, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    finish();
+                }
+            }
+        });
+    }
+}
